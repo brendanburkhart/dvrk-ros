6cdc1099d9c1709f86b4d86f0919beb382c51a8e
Brendan Burkhart <brendan.f.burkhart@gmail.com>
2022-08-11 19:22:59 -0400

    Add RViz arm overlays

--- dvrk_video/scripts/transform_compute.py
+++ dvrk_video/scripts/transform_compute.py
@@ -1,56 +1,70 @@
 #!/usr/bin/python
 
-from visualization_msgs.msg import Marker
-from geometry_msgs.msg import Point, Pose, Quaternion, Vector3, TransformStamped
-from std_msgs.msg import ColorRGBA
+from geometry_msgs.msg import Quaternion, Vector3, Transform, TransformStamped
 import rospy
 import tf2_ros
 import tf_conversions.posemath as pm
-import PyKDL
 import numpy as np
+import rospy
 
-rospy.init_node('transform_compute', anonymous=True)
-tf_buffer = tf2_ros.Buffer()
-listener = tf2_ros.TransformListener(tf_buffer)
-broadcaster = tf2_ros.TransformBroadcaster()
+def frameToMatrix(frame):
+    transform = frame.transform
+    translation = (transform.translation.x, transform.translation.y, transform.translation.z)
+    rotation = (transform.rotation.x, transform.rotation.y, transform.rotation.z, transform.rotation.w)
+    pose = pm.fromTf((translation, rotation))
+    matrix = pm.toMatrix(pose)
+    return matrix
 
-rate = rospy.Rate(10.0)
-camera_to_base = None
-while not rospy.is_shutdown():
-    try:
-        camera_to_tip = tf_buffer.lookup_transform("PSM3", "camera", rospy.Time()).transform
-        t, r = camera_to_tip.translation, camera_to_tip.rotation
-        camera_to_tip = pm.toMatrix(pm.fromTf(((t.x, t.y, t.z), (r.x, r.y, r.z, r.w))))
-        tip_to_base = tf_buffer.lookup_transform("PSM3_base", "PSM3_tool_wrist_link", rospy.Time()).transform
-        t, r = tip_to_base.translation, tip_to_base.rotation
-        tip_to_base = pm.toMatrix(pm.fromTf(((t.x, t.y, t.z), (r.x, r.y, r.z, r.w))))
-    except tf2_ros.LookupException as e:
+def matrixToFrame(matrix):
+    pose = pm.fromMatrix(matrix)
+    translation = pose.p
+    frame = Transform()
+    frame.translation = Vector3(translation.x(), translation.y(), translation.z())
+    (x, y, z, w) = pose.M.GetQuaternion()
+    frame.rotation = Quaternion(x, y, z, w)
+    return frame
+
+def join_transforms():
+    tf_buffer = tf2_ros.Buffer()
+    listener = tf2_ros.TransformListener(tf_buffer)
+    broadcaster = tf2_ros.TransformBroadcaster()
+
+    arm_name = rospy.get_param("~arm")
+    reference = rospy.get_param("~reference")
+    camera = rospy.get_param("~camera")
+    base = "{}_base".format(arm_name)
+
+    rate = rospy.Rate(10.0)
+    robot_to_camera = None
+
+    while not rospy.is_shutdown():
+        try:
+            world_to_tip_frame = tf_buffer.lookup_transform(arm_name, reference, rospy.Time())
+            world_to_tip = frameToMatrix(world_to_tip_frame)
+            tip_to_base_frame = tf_buffer.lookup_transform(base, "{}_tool_wrist_link".format(arm_name), rospy.Time())
+            tip_to_base = frameToMatrix(tip_to_base_frame)
+        except tf2_ros.LookupException as e:
+            rate.sleep()
+            continue
+        except tf2_ros.ConnectivityException as e:
+            rate.sleep()
+            continue
+
+        if robot_to_camera is None:
+            robot_to_camera = TransformStamped()
+            robot_to_camera.child_frame_id = base
+            robot_to_camera.header.frame_id = camera
+
+            x = np.array([[-1, 0, 0, 0], [0, -1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
+            tip_to_base = np.matmul(tip_to_base, x)
+            world_to_base_transform = np.matmul(tip_to_base, world_to_tip) 
+            base_to_world_transform = np.linalg.inv(world_to_base_transform)
+            robot_to_camera.transform = matrixToFrame(base_to_world_transform)
+
+        robot_to_camera.header.stamp = rospy.Time.now()
+        broadcaster.sendTransform(robot_to_camera)
         rate.sleep()
-        continue
-    except tf2_ros.ConnectivityException as e:
-        rate.sleep()
-        continue
 
-    if camera_to_base is None:
-        camera_to_base = TransformStamped()
-        camera_to_base.header.frame_id = "PSM3_base"
-        camera_to_base.child_frame_id = "camera"
-
-        x = np.array([[-1, 0, 0, 0], [0, -1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
-        tip_to_base = np.matmul(tip_to_base, x)
-        camera_to_base_transform = np.matmul(tip_to_base, camera_to_tip) 
-        camera_to_base_frame = pm.fromMatrix(camera_to_base_transform)
-        vec = camera_to_base_frame.p
-        camera_to_base.transform.translation.x = vec.x()
-        camera_to_base.transform.translation.y = vec.y()
-        camera_to_base.transform.translation.z = vec.z()
-        quat = camera_to_base_frame.M.GetQuaternion()
-        camera_to_base.transform.rotation.x = quat[0]
-        camera_to_base.transform.rotation.y = quat[1]
-        camera_to_base.transform.rotation.z = quat[2]
-        camera_to_base.transform.rotation.w = quat[3]
-
-    camera_to_base.header.stamp = rospy.Time.now()
-    broadcaster.sendTransform(camera_to_base)
-    rate.sleep()
-
+if __name__ == "__main__":
+    rospy.init_node("transform_compute")
+    join_transforms()
